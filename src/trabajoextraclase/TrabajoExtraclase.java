324e80081ebdb33a4b965fb4b69f7121e36862ba
Daniela <danicubillo30@gmail.com>
2022-09-24 14:13:23 -0600

    Primer Aporte Trabajo Extraclase

---
+++ src/trabajoextraclase/TrabajoExtraclase.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajoextraclase;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author danic
+ */
+public class TrabajoExtraclase {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Solicitudes al usuario
+        float anti= Float.parseFloat(JOptionPane.showInputDialog(
+                "Digite cuantos años de antiguedad tiene:"));
+        float horas= Float.parseFloat(JOptionPane.showInputDialog(
+                "Ingrese la cantidad de horas laboradas por semana:"));
+        float salario= Float.parseFloat(JOptionPane.showInputDialog(
+                "Ingrese su salario por hora:"));
+        
+        //Cálculos:
+                
+    }
+    
+}
